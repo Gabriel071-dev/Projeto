dd827f042bf13dab08ef0579537fcaa148e001f8
Kaua <kaua010413@gmail.com>
2025-05-25 13:58:48 -0300

    Adicionando padrão mvc no asp .net8

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MaterialdeConstrucao.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
